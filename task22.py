a48d865b634ef53911e6c28ff0ee278a973ae17e
ectomorph <ectomorph312@gmail.com>
2019-12-24 19:00:48 +0600

    task22

---
+++ task22.py
@@ -0,0 +1,8 @@
+#
+# c = int(input('Градус по Фаренгейту: '))
+# result = str((c - 32) * (5/9))
+# print(result[0:5])
+
+c = int(input('Градус по Цельсию: '))
+result = ((9/5) * c + 32)
+print(result)
